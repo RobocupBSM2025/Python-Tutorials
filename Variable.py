873e8a8c8f24ac0d85f5b7e4b13c335193e2e0de
RobocupBSM2025 <RobocupBSM2025@gmail.com>
2025-07-17 13:24:12 -0300

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,38 @@
+String = " Strings are text type variables "
+
+Interger = 1 # A Number Type Variable 
+
+Boolean = type(True) # A true / false varable, you must capitalize the True/False
+
+Float = 1.5 # A number type variable that allows for decimal points (but does not need it)
+
+List = ["String", "Other String", "Only strings, as it can only store one data type in it"]
+List2 = [1, 2, 3, 5]
+List3 = [type(True), Type(False), Type(True)]
+List4 = [1.2, 1.4, 2.3, 1000]
+
+Tuple = ("A list that cannot be modified after creating", "to create one", "I think you need at least one comma")
+
+"""
+There a couple a data types I have no expirence with in python,
+and therefore do no feel qualified enough to explain it to you.
+If you feel you understand these or feel that you can explain anything I have stated better / clearer
+feel free to commit changes to this repository.
+
+These types include :
+
+  Dictionary
+  Set
+  Tuple ~
+  Complex Number
+  Frozenset
+  Range
+  Bytearray
+  Memoryview
+  NoneType
+
+In addition I am not certain how the bool variable. Sorry if this was not the most helpful, python is not
+my main languege.
+"""
+
+
