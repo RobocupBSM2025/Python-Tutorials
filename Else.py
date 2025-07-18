c55bd3e02be67c0a6b23cd30cd072429f42cca1b
RobocupBSM2025 <RobocupBSM2025@gmail.com>
2025-07-18 14:21:02 -0300

    Create Else.py
    For Functions and anything I missed

---
+++ Else.py
@@ -0,0 +1,6 @@
+"""
+
+Other helpfulthings are functions and inputs, but as I have not used them in python at all I will leave those to whoever 
+takes over coding next to explain it, as well as anything else I missed.
+
+"""
